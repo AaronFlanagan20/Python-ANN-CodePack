e2517882191366ff4c6426ab7b6d7fb93301fafa
AaronFlanagan20 <aaronflanagan044@hotmail.com>
2020-06-14 18:06:25 +0100

    metrics using scikit-learn, added .gitignore for IDEA

--- binary_classificiation_problem.py
+++ binary_classificiation_problem.py
@@ -1,30 +1,78 @@
 # credit: https://machinelearningmastery.com/how-to-calculate-precision-recall-f1-and-more-for-deep-learning-models/
-
-# Example of generating samples from the two circle problem
+# demonstration of calculating metrics for a neural network model using sklearn
 from sklearn.datasets import make_circles
-from matplotlib import pyplot
-from numpy import where
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential
+from keras.layers import Dense
 
-# generate 2d classification dataset
-X, y = make_circles(n_samples=1000, noise=0.1, random_state=1)
-# scatter plot, dots colored by class value
-for i in range(2):
-    samples_ix = where(y == i)
-    pyplot.scatter(X[samples_ix, 0], X[samples_ix, 1])
 
-# pyplot.show()
+# generate and prepare the dataset
+def get_data():
+    # generate dataset
+    X, y = make_circles(n_samples=1000, noise=0.1, random_state=1)
+    # split into train and test
+    n_test = 500
+    trainX, testX = X[:n_test, :], X[n_test:, :]
+    trainy, testy = y[:n_test], y[n_test:]
+    return trainX, trainy, testX, testy
 
-# numpy build Multilayer Perceptron
 
-# split into train and test
-n_test = 500
-XTrain, testX = X[:n_test, :], X[n_test:, :]
-trainy, testy = y[:n_test], y[n_test:]
+# define and fit the model
+def get_model(trainX, trainy):
+    # define model
+    model = Sequential()
+    model.add(Dense(100, input_dim=2, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit model
+    model.fit(trainX, trainy, epochs=300, verbose=0)
+    return model
 
-# define model
-model = Sequential()
-model.add(Dense(100, input_dim=2, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
 
-# compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# generate data
+trainX, trainy, testX, testy = get_data()
+# fit model
+model = get_model(trainX, trainy)
+
+# predict probabilities for test set
+yhat_probs = model.predict(testX, verbose=0)
+# predict crisp classes for test set
+yhat_classes = model.predict_classes(testX, verbose=0)
+# reduce to 1d array
+yhat_probs = yhat_probs[:, 0]
+yhat_classes = yhat_classes[:, 0]
+
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(testy, yhat_classes)
+print('Accuracy: %f' % accuracy)
+
+# precision tp / (tp + fp)
+precision = precision_score(testy, yhat_classes)
+print('Precision: %f' % precision)
+
+# recall: tp / (tp + fn)
+recall = recall_score(testy, yhat_classes)
+print('Recall: %f' % recall)
+
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(testy, yhat_classes)
+print('F1 score: %f' % f1)
+
+# kappa
+kappa = cohen_kappa_score(testy, yhat_classes)
+print('Cohens kappa: %f' % kappa)
+
+# ROC AUC
+auc = roc_auc_score(testy, yhat_probs)
+print('ROC AUC: %f' % auc)
+
+# confusion matrix
+matrix = confusion_matrix(testy, yhat_classes)
+print(matrix)
