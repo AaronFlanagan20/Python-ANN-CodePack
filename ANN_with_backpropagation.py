f4f0458118d9bb112d12857f73e4552593cd3495
AaronFlanagan20 <aaronflanagan044@hotmail.com>
2020-06-14 16:35:23 +0100

    accuracy metrics, cross-validation and data normalization added

--- ANN_with_backpropagation.py
+++ ANN_with_backpropagation.py
@@ -1,18 +1,97 @@
 # credit: https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
 
-from math import exp
 from random import seed
+from random import randrange
 from random import random
+from csv import reader
+from math import exp
 
 
-# Initialize a network
-def initialize_network(n_inputs, n_hidden, n_outputs):
-    network = list()
-    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
-    network.append(hidden_layer)
-    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
-    network.append(output_layer)
-    return network
+# Load a CSV file
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+    minmax = list()
+    stats = [[min(column), max(column)] for column in zip(*dataset)]
+    return stats
+
+
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+    for row in dataset:
+        for i in range(len(row) - 1):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
 
 
 # Calculate neuron activation for an input
@@ -28,11 +107,6 @@ def transfer(activation):
     return 1.0 / (1.0 + exp(-activation))
 
 
-# Calculate the derivative of an neuron output using sigmoid function
-def transfer_derivative(output):
-    return output * (1.0 - output)
-
-
 # Forward propagate input to a network output
 def forward_propagate(network, row):
     inputs = row
@@ -44,6 +118,11 @@ def forward_propagate(network, row):
             new_inputs.append(neuron['output'])
         inputs = new_inputs
     return inputs
+
+
+# Calculate the derivative of an neuron output using sigmoid function
+def transfer_derivative(output):
+    return output * (1.0 - output)
 
 
 # Backpropagate error and store in neurons
@@ -68,6 +147,7 @@ def backward_propagate_error(network, expected):
             neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
 
 
+# Update network weights with error
 def update_weights(network, row, l_rate):
     for i in range(len(network)):
         inputs = row[:-1]
@@ -94,32 +174,56 @@ def train_network(network, train, l_rate, n_epoch, n_outputs):
         print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
 
 
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
+
+
 # Make a prediction with a network
 def predict(network, row):
     outputs = forward_propagate(network, row)
     return outputs.index(max(outputs))  # print index of highest value in list
 
 
-# test back-propagation of error
+# Backpropagation Algorithm With Stochastic Gradient Descent
+def back_propagation(train, test, l_rate, n_epoch, n_hidden):
+    n_inputs = len(train[0]) - 1
+    n_outputs = len(set([row[-1] for row in train]))
+    network = initialize_network(n_inputs, n_hidden, n_outputs)
+    train_network(network, train, l_rate, n_epoch, n_outputs)
+    predictions = list()
+    for row in test:
+        prediction = predict(network, row)
+        predictions.append(prediction)
+    return (predictions)
+
+
+# Test Backprop on Seeds dataset
 seed(1)
-dataset = [[2.7810836, 2.550537003, 0],
-           [1.465489372, 2.362125076, 0],
-           [3.396561688, 4.400293529, 0],
-           [1.38807019, 1.850220317, 0],
-           [3.06407232, 3.005305973, 0],
-           [7.627531214, 2.759262235, 1],
-           [5.332441248, 2.088626775, 1],
-           [6.922596716, 1.77106367, 1],
-           [8.675418651, -0.242068655, 1],
-           [7.673756466, 3.508563011, 1]]
+# load and prepare data
+filename = 'data/wheat-seeds.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])-1):
+    str_column_to_float(dataset, i)
 
-n_inputs = len(dataset[0]) - 1
-n_outputs = len(set([row[-1] for row in dataset]))  # (0,1)
-network = initialize_network(n_inputs, 2, n_outputs)  # inputs, 2 hidden neurons, outputs
-train_network(network, dataset, 0.65, 1000, n_outputs)  # error=0.003 - adjust if more training samples added
+# convert class column to integers
+str_column_to_int(dataset, len(dataset[0])-1)
 
-print('\nMaking Predictions: ')
-for row in dataset:
-    prediction = predict(network, row)
-    print('Expected=%d, Got=%d' % (row[-1], prediction))
+# normalize input variables
+minmax = dataset_minmax(dataset)
+normalize_dataset(dataset, minmax)
 
+# evaluate algorithm
+n_folds = 5
+l_rate = 0.3
+n_epoch = 500
+n_hidden = 5
+
+scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
